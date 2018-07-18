aa174b8cc269981801ae30c647df71c097cabc3f
goreleaserbot <goreleaser@carlosbecker.com>
2018-07-17 17:16:13 -0700

    Brew formula update for pinpoint-agent version v0.26.0

--- Formula/pinpoint-agent.rb
+++ Formula/pinpoint-agent.rb
@@ -1,9 +1,9 @@
 class PinpointAgent < Formula
   desc "Pinpoint Agent is the software that runs on customer-premise to collect and deliver performance details to the Pinpoint Cloud"
   homepage "https://pinpt.com"
-  url "https://github.com/pinpt/agent/releases/download/v0.25.0/pinpoint-agent_0.25.0_darwin_amd64.tar.gz"
-  version "0.25.0"
-  sha256 "3cc375e12787dca23f4a03c4ad15c2e5b2019d399f727415b74c465f8e8d23c3"
+  url "https://github.com/pinpt/agent/releases/download/v0.26.0/pinpoint-agent_0.26.0_darwin_amd64.tar.gz"
+  version "0.26.0"
+  sha256 "87b666d338fca4760c793daae249466c4a3ef7201da4acd9dc32a8afb04d1b86"
 
   def install
     bin.install "pinpoint-agent"
