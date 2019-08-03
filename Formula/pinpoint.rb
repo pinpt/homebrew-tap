b239b6fdc65a2f4c8c67c69d1578fd12c3b4c7b0
goreleaserbot <goreleaser@carlosbecker.com>
2019-08-02 22:05:23 -0500

    Brew formula update for pinpoint version 0.0.15

--- Formula/pinpoint.rb
+++ Formula/pinpoint.rb
@@ -2,14 +2,14 @@
 class Pinpoint < Formula
   desc "Pinpoint CI Tool is a utility to collect and deliver CI/CD details to the Pinpoint Cloud"
   homepage "https://pinpoint.com"
-  version "0.0.14"
+  version "0.0.15"
 
   if OS.mac?
-    url "https://github.com/pinpt/pinpoint/releases/download/0.0.14/pinpoint_0.0.14_Darwin_x86_64.tar.gz"
-    sha256 "1073b8c451397634eded100e2b012d6fabcbbcaf96967d059334bd0d62ce827e"
+    url "https://github.com/pinpt/pinpoint/releases/download/0.0.15/pinpoint_0.0.15_Darwin_x86_64.tar.gz"
+    sha256 "b543996ff3351fd4282ba1004c8bf57aa3381be6f729d4836e6342bc976f5586"
   elsif OS.linux?
-    url "https://github.com/pinpt/pinpoint/releases/download/0.0.14/pinpoint_0.0.14_Linux_x86_64.tar.gz"
-    sha256 "1cda3700e3c97e9bfb2cf6cc0d6dbe0dc4f91fbfed751752770dae5d931b90a4"
+    url "https://github.com/pinpt/pinpoint/releases/download/0.0.15/pinpoint_0.0.15_Linux_x86_64.tar.gz"
+    sha256 "49f7748dbcfc50783a1daa5c660ccc39f5772dea6fa436d3be9b3ae0e84b7154"
   end
 
   def install
